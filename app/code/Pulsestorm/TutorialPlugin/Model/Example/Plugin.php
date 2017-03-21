a994f3b75900c84842827347961bbe02d7574a9c
ProjectEight <s.frost@frostnet.co.uk>
2017-03-21 18:06:18 +0000

    Updated example extension to demonstrate how after and around plugins work in article 8 (See http://alanstorm.com/magento_2_object_manager_plugin_system/)

--- app/code/Pulsestorm/TutorialPlugin/Model/Example/Plugin.php
+++ app/code/Pulsestorm/TutorialPlugin/Model/Example/Plugin.php
@@ -10,14 +10,69 @@ namespace Pulsestorm\TutorialPlugin\Model\Example;
 
 class Plugin
 {
-	public function beforeGetMessage( $subject, $thing = 'World', $should_lc = false )
-	{
+	/**
+	 * Call the plugin before the original target method has run
+	 *
+	 * @param \Pulsestorm\TutorialPlugin\Model\Example  $subject   The class which contains the original target method
+	 * @param string                                    $thing     First parameter to the original target method
+	 * @param bool                                      $should_lc Second parameter to the original target method
+	 *
+	 * @return array
+	 */
+//	public function beforeGetMessage( $subject, $thing = 'World', $should_lc = false )
+//	{
 		// Change the first parameter ($thing), but pass through the second one ($should_lc) unchanged
-		return ['Changing the argument', $should_lc];
-	}
+//		return ['Changing the argument', $should_lc];
+//	}
 
+	/**
+	 * Call the plugin after the original target method has run
+	 *
+	 * @param \Pulsestorm\TutorialPlugin\Model\Example  $subject   The class which contains the original target method
+	 * @param string                                    $result
+	 *
+	 * @return string
+	 */
 //	public function afterGetMessage( $subject, $result )
 //	{
 //		return 'We are so tired of saying hello';
 //	}
+
+	/**
+	 * Call the function before and after the target method
+	 *
+	 * @param \Pulsestorm\TutorialPlugin\Model\Example  $subject   The class which contains the original target method
+	 * @param callback                                  $proceed   Callback which executes the original target method
+	 * @param string                                    $thing     First parameter to the original target method
+	 * @param bool                                      $should_lc Second parameter to the original target method
+	 *
+	 * @return mixed
+	 */
+//	public function aroundGetMessage( $subject, $proceed, $thing = 'World', $should_lc = false )
+//	{
+//		echo 'Calling' . __METHOD__ . ' before', "\n";
+//		$result = $proceed();
+//		echo 'Calling' . __METHOD__ . ' after', "\n";
+//
+//		return $result;
+//	}
+
+	/**
+	 * Bypass the original target method and return a custom value
+	 *
+	 * @param \Pulsestorm\TutorialPlugin\Model\Example  $subject   The class which contains the original target method
+	 * @param callback                                  $proceed   Callback which executes the original target method
+	 * @param string                                    $thing     First parameter to the original target method
+	 * @param bool                                      $should_lc Second parameter to the original target method
+	 *
+	 * @return mixed
+	 */
+	public function aroundGetMessage( $subject, $proceed, $thing = 'World', $should_lc = false )
+	{
+		echo 'Calling' . __METHOD__ . ' before', "\n";
+//		$result = $proceed();
+		echo 'Calling' . __METHOD__ . ' after', "\n";
+
+		return 'New return value';
+	}
 }
